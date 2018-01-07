a622c5b3fef3b471960ccb5d8eae034f3a86d252
ZAUB1 <aubin.luneau@icloud.com>
2018-01-07 19:51:24 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,2 +1,2 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-(50, 'pizza', 'Pizza', -1, 0, 1),
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('pizza', 'Pizza', -1, 0, 1),
